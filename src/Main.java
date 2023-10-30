420604976a4c31f26ffcc6cc2fd4b869dd40cccd
dado-offbrand <dogeychicken47@gmail.com>
2023-10-30 09:06:21 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+public class Main {
+    public static void main(String[] args) {
+        for (int i = 0; i <= 30; i++) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+
+        for (int i = 30; i >= 0; i--) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+
+        for (int i = 0; i <= 18; i+=3) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+
+        for (int i = 10; i >= 0; i-=2) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+    }
+}
